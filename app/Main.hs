ee08ba175c255f05b68134ed06099f34b1da69d9
tobias <buecktobias@gmail.com>
2020-04-30 18:26:26 +0200

    Position type

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,13 @@
 module Main where
-import Day1
-import Day2
+import Day3
 import Lib
 
 main :: IO ()
 main = do
-  contents <- readFile "input/input2.txt"
-  let list = split ',' contents
-  let intList = map Lib.readInt list
-  -- let result = Day2.resultIntProgramNounVerb intList 12 2
-  let nounVerb = bruteForceIntProgram intList 19690720
-  let noun = fst nounVerb
-  let verb = snd nounVerb
-  print (100 * noun + verb)
+  contents <- readFile "input/input3.txt"
+  let list = split '\n' contents
+  let lines = map (\line -> split ',' line) list
+  print lines
+  let pos = Position {x=3, y=3}
+  print pos
 
---
+++ src/Day3.hs
@@ -0,0 +1,5 @@
+module Day3 (Position(..)) where
+
+data Position = Position{ x :: Int,
+                         y :: Int
+                     } deriving (Show)
